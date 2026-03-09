f6966817fd0389926f67d3ae124dd2be10500b03
Delean Mafra <170483770+Delean-Mafra@users.noreply.github.com>
2026-03-08 22:25:33 -0300

    Update Kaggle credentials handling in pex_4.py

--- pex_4.py
+++ pex_4.py
@@ -3,10 +3,11 @@ import kagglehub
 
 # Defina suas credenciais diretamente no código
 os.environ["KAGGLE_USERNAME"] = "deleanmafra"
-os.environ["KAGGLE_KEY"] = "KGAT_bb8352c6d7e844497f01461e9e54b0b4"  # aqui você coloca sua chave real
+os.environ["KAGGLE_KEY"] = ""  
 
 handle = "deleanmafra/seu_dataset"
 local_dataset_dir = "path/to/local/dataset/dir"
 
 # Criar ou atualizar dataset
+
 kagglehub.dataset_upload(handle, local_dataset_dir)
